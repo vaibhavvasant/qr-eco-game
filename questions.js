7af53cdbb60fdc425b45d276e43acd537a115092
vaibhavvasant <vaibhavvasant7@gmail.com>
2025-09-16 15:47:12 +0530

    functioning quiz pages (desktop)

---
+++ questions.js
@@ -0,0 +1,29 @@
+const quizData = {
+  tree: [
+    {
+      question: "Which tree is known as the 'Tree of Life' in desert regions?",
+      options: ["Coconut Tree", "Date Palm", "Neem Tree", "Banyan Tree"],
+      answer: 1
+    },
+    {
+      question: "What process in trees helps reduce carbon dioxide in the air?",
+      options: ["Transpiration", "Respiration", "Photosynthesis", "Combustion"],
+      answer: 2
+    },
+    {
+      question: "India’s national tree is the:",
+      options: ["Peepal Tree", "Banyan Tree", "Neem Tree", "Mango Tree"],
+      answer: 1
+    },
+    {
+      question: "Which part of the tree carries water from roots to leaves?",
+      options: ["Bark", "Xylem", "Phloem", "Branches"],
+      answer: 1
+    },
+    {
+      question: "Planting trees along roads is mainly done to:",
+      options: ["Beautify streets", "Reduce dust & pollution", "Provide wood", "Prevent accidents"],
+      answer: 1
+    }
+  ]
+};
