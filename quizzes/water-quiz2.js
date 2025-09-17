dfe055fe2c4d31c77f96f9974dc6cbe6ffafa0d6
diauh <diya120225@gmail.com>
2025-09-17 14:38:28 +0530

    Update water-quiz2.js

--- quizzes/water-quiz2.js
+++ quizzes/water-quiz2.js
@@ -23,31 +23,6 @@ const NEW_QUESTIONS = [
     q: `Which gas is the primary cause of acid rain?`,
     opts: ["Carbon Monoxide", "Sulfur Dioxide", "Methane", "Helium"],
     correct: 1
-  },
-  {
-    q: `What percentage of the Earth’s surface is covered by oceans?`,
-    opts: ["50%", "71%", "30%", "85%"],
-    correct: 1
-  },
-  {
-    q: `Which planet is known as the “Red Planet”?`,
-    opts: ["Mars", "Venus", "Jupiter", "Mercury"],
-    correct: 0
-  },
-  {
-    q: `Which process in plants releases oxygen into the air?`,
-    opts: ["Respiration", "Photosynthesis", "Transpiration", "Combustion"],
-    correct: 1
-  },
-  {
-    q: `Which natural phenomenon is measured by the Richter scale?`,
-    opts: ["Hurricanes", "Earthquakes", "Tornadoes", "Volcanic eruptions"],
-    correct: 1
-  },
-  {
-    q: `Which type of rock is formed from cooled lava or magma?`,
-    opts: ["Sedimentary", "Igneous", "Metamorphic", "Limestone"],
-    correct: 1
   }
 ];
 ALL_QUESTIONS.push(...NEW_QUESTIONS);
