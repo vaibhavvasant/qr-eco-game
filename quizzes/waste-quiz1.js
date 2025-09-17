f98c460e8c418918f1ce86eb35b109873f2b0db0
diauh <diya120225@gmail.com>
2025-09-17 14:48:56 +0530

    Create waste-quiz1.js

---
+++ quizzes/waste-quiz1.js
@@ -0,0 +1,37 @@
+const WASTE_QUESTIONS = [
+  {
+    q: `More people in the world have access to mobile phones than to clean toilets.`,
+    opts: ["True", "False"],
+    correct: 0
+  },
+  {
+    q: `What color bin is usually used for biodegradable waste in India?`,
+    opts: ["Green", "Blue", "Black", "Red"],
+    correct: 0
+  },
+  {
+    q: `Which gas is released from landfills when waste decomposes?`,
+    opts: ["Oxygen", "Methane", "Helium", "Carbon Monoxide"],
+    correct: 1
+  },
+  {
+    q: `You buy a large soda but can’t finish it. What’s the most eco-friendly thing to do with the leftover drink?`,
+    opts: [
+      "Pour it down the sink",
+      "Throw the cup + drink in the trash",
+      "Share with someone or store in fridge",
+      "Pour it in a garden as compost water"
+    ],
+    correct: 2
+  },
+  {
+    q: `The concept of a “circular economy” means:`,
+    opts: [
+      "Throwing waste in a circular landfill",
+      "Reusing materials to reduce waste",
+      "Selling waste back to factories",
+      "Recycling only plastics"
+    ],
+    correct: 1
+  }
+  ]
