9073278b1b3bdc641700140db5fb2dd681a71078
diauh <diya120225@gmail.com>
2025-09-17 14:42:17 +0530

    Create water-quiz2.js

---
+++ quizzes/water-quiz2.js
@@ -0,0 +1,29 @@
+
+const NEW_QUESTIONS = [
+{
+    q: `What percentage of the Earth’s surface is covered by oceans?`,
+    opts: ["50%", "71%", "30%", "85%"],
+    correct: 1
+  },
+  {
+    q: `Which planet is known as the “Red Planet”?`,
+    opts: ["Mars", "Venus", "Jupiter", "Mercury"],
+    correct: 0
+  },
+  {
+    q: `Which process in plants releases oxygen into the air?`,
+    opts: ["Respiration", "Photosynthesis", "Transpiration", "Combustion"],
+    correct: 1
+  },
+  {
+    q: `Which natural phenomenon is measured by the Richter scale?`,
+    opts: ["Hurricanes", "Earthquakes", "Tornadoes", "Volcanic eruptions"],
+    correct: 1
+  },
+  {
+    q: `Which type of rock is formed from cooled lava or magma?`,
+    opts: ["Sedimentary", "Igneous", "Metamorphic", "Limestone"],
+    correct: 1
+  }
+];
+ALL_QUESTIONS.push(...NEW_QUESTIONS);
