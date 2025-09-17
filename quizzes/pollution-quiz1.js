9f3d5831c93b7e5d02ba3bb0beab04dbd1d510ff
diauh <diya120225@gmail.com>
2025-09-17 14:54:15 +0530

    Create pollution-quiz1.js

---
+++ quizzes/pollution-quiz1.js
@@ -0,0 +1,37 @@
+const POLLUTION_QUESTIONS = [
+  {
+    q: `Which pollutant causes smog in cities?`,
+    opts: ["Nitrogen Oxides", "Helium", "Hydrogen", "Oxygen"],
+    correct: 0
+  },
+  {
+    q: `In cities, planting trees along roads can help reduce:`,
+    opts: ["Plastic waste", "Sound and dust", "Ocean pollution", "Food wastage"],
+    correct: 1
+  },
+  {
+    q: `Which everyday product contributes the most to microplastic pollution in oceans?`,
+    opts: [
+      "Plastic straws",
+      "Synthetic clothes when washed",
+      "Glass bottles",
+      "Cotton bags"
+    ],
+    correct: 1
+  },
+  {
+    q: `Which of these is the least obvious but still harmful type of pollution?`,
+    opts: [
+      "Noise from headphones",
+      "Radioactive waste",
+      "Street litter",
+      "Industrial smoke"
+    ],
+    correct: 1
+  },
+  {
+    q: `Which country is famous for building vertical forests (towers with thousands of trees) to reduce city pollution?`,
+    opts: ["Japan", "Italy", "China", "Brazil"],
+    correct: 1
+  }
+  ]
