c5e594ab6c728086ddb17d4320894c22e776faad
vaibhavvasant <vaibhavvasant7@gmail.com>
2025-09-18 15:21:27 +0530

    popup bug fixed, all quiz pages working

--- quizzes/awardCharacter.js
+++ quizzes/awardCharacter.js
@@ -1,49 +1,50 @@
 // Award character on quiz completion
 function awardCharacter(type) {
-  // Roll rarity probability
   const roll = Math.random() * 100;
   let rarity;
   if (roll < 60) rarity = "common";
   else if (roll < 90) rarity = "uncommon";
   else rarity = "rare";
 
-  // Character pool
   const characters = {
     tree: {
-      common: ["tree_common.jpg"],
-      uncommon: ["tree_uncommon.jpg"],
-      rare: ["tree_rare.png"]
+      common: ["../characters/tree_common.jpg"],
+      uncommon: ["../characters/tree_uncommon.jpg"],
+      rare: ["../characters/tree_rare.png"]
     },
     water: {
-      common: ["water_common.jpg"],
-      uncommon: ["water_uncommon.jpg"],
-      rare: ["water_rare.jpg"]
+      common: ["../characters/water_common.jpg"],
+      uncommon: ["../characters/water_uncommon.jpg"],
+      rare: ["../characters/water_rare.jpg"]
     },
     waste: {
-      common: ["waste_common.jpg", "waste_common.png"],
-      uncommon: ["waste_uncommon.jpg", "waste_uncommon.png"],
-      rare: ["waste_rare.jpg"]
+      common: ["../characters/waste_common.jpg"],
+      uncommon: ["../characters/waste_uncommon.jpg"],
+      rare: ["../characters/waste_rare.jpg"]
     },
     pollution: {
-      common: ["pollution_common.jpg"],
-      uncommon: ["pollution_uncommon.jpg"],
-      rare: ["pollution_rare.jpg"]
+      common: ["../characters/pollution_common.jpg"],
+      uncommon: ["../characters/pollution_uncommon.jpg"],
+      rare: ["../characters/pollution_rare.jpg"]
     }
   };
 
   const pool = characters[type][rarity];
   const selected = pool[Math.floor(Math.random() * pool.length)];
 
-  // Progress in localStorage
   const collection = JSON.parse(localStorage.getItem("characterCollection") || "{}");
 
   if (!collection[selected]) {
-    collection[selected] = { type, rarity, level: 0 }; // new unlock
+    collection[selected] = { type, rarity, level: 1 }; // new unlock starts at level 1
   } else {
     collection[selected].level += 1; // level up
   }
 
   localStorage.setItem("characterCollection", JSON.stringify(collection));
 
-  return { character: selected, rarity, level: collection[selected].level };
+  return {
+    character: selected, // filename
+    rarity,
+    level: collection[selected].level
+  };
 }
